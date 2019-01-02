a59813ffe119464430be392a2859d0c078b2a1d7
ByungjuKim <45156153+feidfoe@users.noreply.github.com>
2019-01-02 16:34:40 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,13 +2,12 @@
 gpuid=0
 
 
-
 nvidia-docker run -v `pwd`:`pwd` -w `pwd` --rm \
                   --name 'mnist_color_baseline_gpu'${gpuid} \
                   -e CUDA_VISIBLE_DEVICES=${gpuid} \
                   -it \
                   --ipc=host \
                   feidfoe/pytorch:latest \
-                  python main.py -e unlearn_0.02  --color_var 0.020 --lr 0.001 --use_pretrain --checkpoint checkpoint/baseline_0.02/checkpoint_step_0100.pth --cuda --is_train --data_split train
+                  python main.py -e unlearn_0.02  --color_var 0.020 --lr 0.001 --use_pretrain --checkpoint path/to/pretrained/model.pth --cuda --is_train --data_split train
 
 
